c2ae7edacf274f526da6932cd7c7d09fe42ff1f6
Jessi De Pin <lilijess.yo@gmail.com>
2016-02-20 21:56:59 -0430

    Omega
    omega

---
+++ menu.php
@@ -0,0 +1,85 @@
+<div class="ContenedorMenu TextoCentrado LetraNegrita">
+        
+                <div id="header" class="col-md-12 LetraEstilizada EspacioBlancoArriba">
+                           <div class="col-md-2">
+                               
+                           <img src="img/LogoOmega.gif" alt="">
+                       </div>
+                           <ul class="nav col-md-10" >
+
+                                <li><a href="">Inicio</a></li>
+                                <li><a href="">Compañia</a>
+                                        <ul>
+                                                <li><a href="">Quienes Somos</a></li>
+                                                <li><a href="">Trabaje con Nosotros</a></li>
+                                         </ul>
+                                </li>   
+                                <li><a href="">Soluciones</a>
+                                         <ul>
+                                                <li><a href="">Omega Service Desk</a>
+
+                                                        <ul>
+                                                                <li><a href="">Requerimientos de Servicio</a></li>
+                                                                <li><a href="">Incidentes</a></li>
+                                                                <li><a href="">Ploblemas</a></li>
+                                                                <li><a href="">Cambios</a></li>
+                                                                <li><a href="">Catalogo de Servicios</a></li>
+                                                                <li><a href="">Acuerdos de Niveles de Servicios</a></li>
+                                                                <li><a href="">Proyectos</a></li>
+                                                        </ul>
+                                                </li>
+                                                <li><a href="">Omega CMDB</a>
+
+                                                        <ul>
+                                                                <li><a href="">Activos</a></li>
+                                                                <li><a href="">Contratos</a></li>
+                                                                <li><a href="">Mantenimiento</a></li> 
+                                                        </ul>
+                                                </li>
+                                                <li><a href="">Omega Knowlegde <br> Base Articulos</a></li>
+
+                                                        
+                                         </ul>
+                                </li>
+
+                                 <li><a href="">Servicios</a>
+                                        <ul>
+                                                <li><a href="">Consultoria de Procesos</a></li>
+                                                <li><a href="">Desarrollo de Software</a></li>
+                                                <li><a href="">Otros Definir</a></li>
+
+                                         </ul>
+                                </li> 
+
+                                 <li><a href="">Soporte</a>
+                                        <ul>
+                                                <li><a href="">Solicitud de Soporte</a></li>
+                                                <li><a href="">Wiki</a></li>
+                                         </ul>
+                                </li> 
+                                 <li><a href="">Clientes</a>
+                                        <ul>
+                                                <li><a href="">Nuestros Clientes</a></li>
+                                                <li><a href="">Casos de Exitos</a></li>
+                                         </ul>
+                                </li> 
+                                 <li><a href="">Partners</a>
+                                        <ul>
+                                                <li><a href="">Programa de Partners</a></li>
+                                         </ul>
+                                </li> 
+                              <li><a href="">Comunidad</a>
+                                        <ul>
+                                                <li><a href="">Blog</a></li>
+                                                <li><a href="">Webcast</a></li>
+                                                <li><a href="">Noticias</a></li>
+
+                                         </ul>
+                                </li>
+                                <li><a href="">Solicitud de Demos</a></li>
+
+                                <li><a href="">Contactos</a></li>
+                        </ul>
+                </div>
+
+</div>
